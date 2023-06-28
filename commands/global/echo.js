606aff65a41148c1ad174cda7db8d2f441cf09eb
Ondřej Schlaichert <schlaichert.o@gmail.com>
2023-06-28 17:34:58 +0200

    Changed echo to be a slash command

---
+++ commands/global/echo.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('echo')
+		.setDescription('Repeats whatever you write.')
+		.addStringOption(option =>
+			option
+				.setName('content')
+				.setDescription('The content of the echoed message.')
+				.setRequired(true))
+		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+	async execute(interaction) {
+		const content = interaction.options.getString('content') ?? null;
+
+		if (content == null) {
+			await interaction.reply({ content: 'No content provided.', ephemeral: true });
+			return;
+		}
+
+		const channel = interaction.guild.channels.cache.get(interaction.channelId);
+		channel.send(content);
+		await interaction.reply({ content: 'Done!', ephemeral: true });
+	},
+};
--- commands/global/ping.js
+++ commands/global/ping.js
@@ -3,7 +3,8 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('A simple testing command.'),
+		.setDescription('A simple testing command.')
+		.setDMPermission(true),
 	async execute(interaction) {
 		await interaction.reply(`Ho, ${interaction.member.displayName}! So you're still alive.`);
 	},
