01a3b8801210e3ead30c098899898066d37c69a6
Ondřej Schlaichert <schlaichert.o@gmail.com>
2023-07-02 17:37:17 +0200

    Added the /setwelcomechannel and /delwelcomechannel commands

---
+++ commands/global/delwelcomechannel.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const welcomeChannels = require('../../helper_scripts/welcomeChannels.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('delwelcomechannel')
+		.setDescription('Unset this channel as a welcome channel.')
+		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+		.setDMPermission(false),
+	async execute(interaction) {
+		console.log(`[LOG] @${interaction.user.username} used the /delwelcomechannel command.`);
+
+		let welcomeChannelArray = welcomeChannels.getWelcomeChannelsFromJson();
+
+		if (!welcomeChannelArray.includes(interaction.channelId)) {
+			await interaction.reply({
+				content:
+					'This channel is not a welcome channel.'
+				, ephemeral: true
+			});
+			return;
+		}
+
+		welcomeChannelArray.splice(welcomeChannelArray.indexOf(interaction.channelId), 1);
+
+		welcomeChannels.saveWelcomeChannelsToJson(welcomeChannelArray);
+
+		interaction.reply({ content: 'Unset this channel as a welcome channel.', ephemeral: true });
+	},
+};
---
+++ commands/global/setwelcomechannel.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const welcomeChannels = require('../../helper_scripts/welcomeChannels.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('setwelcomechannel')
+		.setDescription('Set this channel as a welcome channel. Methuselah will welcome people who join here.')
+		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+		.setDMPermission(false),
+	async execute(interaction) {
+		console.log(`[LOG] @${interaction.user.username} used the /setwelcomechannel command.`);
+
+		let welcomeChannelArray = welcomeChannels.getWelcomeChannelsFromJson();
+
+		if (welcomeChannelArray.includes(interaction.channelId)) {
+			await interaction.reply({
+				content:
+					'This channel is already a welcome channel.'
+				, ephemeral: true
+			});
+			return;
+		}
+
+		welcomeChannelArray.push(interaction.channelId);
+
+		welcomeChannels.saveWelcomeChannelsToJson(welcomeChannelArray);
+
+		interaction.reply({ content: 'Welcome channel set!', ephemeral: true });
+	},
+};
