24bd5b029d5b1a0714d893d3a232c07200fd8ad5
Ondřej Schlaichert <schlaichert.o@gmail.com>
2023-07-05 21:16:44 +0200

    Fixed adding multiple permamessages

--- commands/global/setpermamessage.js
+++ commands/global/setpermamessage.js
@@ -29,14 +29,12 @@ module.exports = {
 		let permamessageMap = permamessages.getPermamessageMapFromJson();
 
 		if (permamessageMap.has(interaction.channelId)) {
-			if (!permamessageMap.has(interaction.channelId)) {
-				const embed = new EmbedBuilder()
-					.setDescription(
-						'**✕** this channel already has a permamessage. Use /delpermamessage first to replace it.')
-					.setColor(Colors.Red);
-				interaction.reply({ embeds: [embed], ephemeral: true });
-				return;
-			}
+			const embed = new EmbedBuilder()
+				.setDescription(
+					'**✕** this channel already has a permamessage. Use /delpermamessage first to replace it.')
+				.setColor(Colors.Red);
+			interaction.reply({ embeds: [embed], ephemeral: true });
+			return;
 		}
 
 		const channel = interaction.guild.channels.cache.get(interaction.channelId);
