0c2caddaf0c06117fa3862ea52b67f623d8a7670
Ondřej Schlaichert <schlaichert.o@gmail.com>
2023-07-05 17:37:12 +0200

    Added three second cooldown to permamessages

--- events/permamessage.js
+++ events/permamessage.js
@@ -1,10 +1,13 @@
-const { Events } = require('discord.js');
+const { Events, Collection } = require('discord.js');
 const { clientId } = require('../config.json');
 const permamessages = require('../helper_scripts/permamessages.js');
+const { cooldown } = require('../commands/global/setpermamessage');
 
 module.exports = {
 	name: Events.MessageCreate,
 	async execute(message) {
+		const permamessageDelay = 3000;
+
 		let permamessageMap = permamessages.getPermamessageMapFromJson();
 
 		if (!permamessageMap.has(message.channelId)) return;
@@ -19,16 +22,27 @@ module.exports = {
 			console.error(e);
 		});
 
-		if (typeof lastMessage != 'undefined' && lastMessageId != null && lastMessage.author.id == clientId) {
-			lastMessage.delete().catch(e => {
-				console.error('[ERROR] Permamessage message deletion wasn\'t successful.');
-				console.error(e);
-				return;
-			});
+		const { sentPermamessages } = message.client;
+
+		// Clear planned timeout if it exists
+		if (sentPermamessages.has(message.channelId)) {
+			clearTimeout(sentPermamessages.get(message.channelId));
+			sentPermamessages.delete(message.channelId);
 		}
 
-		let sentMessage = await message.channel.send(permamessageMap.get(message.channelId).content);
-		permamessageMap.get(message.channelId).sentMessageId = sentMessage.id;
-		permamessages.savePermamessageMapToJson(permamessageMap);
+		if (typeof lastMessage != 'undefined' && lastMessageId != null && lastMessage.author.id == clientId) {
+			sentPermamessages.set(message.channelId, setTimeout(async () => {
+				sentPermamessages.delete(message.channelId);
+				lastMessage.delete().catch(e => {
+					console.error('[ERROR] Permamessage message deletion wasn\'t successful.');
+					console.error(e);
+					return;
+				});
+
+				let sentMessage = await message.channel.send(permamessageMap.get(message.channelId).content);
+				permamessageMap.get(message.channelId).sentMessageId = sentMessage.id;
+				permamessages.savePermamessageMapToJson(permamessageMap);
+			}, permamessageDelay));
+		}
 	}
 };
