99620d040bb4dfbc5dc1295492ed2675600784bf
Ondřej Schlaichert <schlaichert.o@gmail.com>
2024-06-18 22:21:16 +0200

    Old changes

--- events/memberRemovedReaction.js
+++ events/memberRemovedReaction.js
@@ -3,19 +3,16 @@ const reactionRoleMessagesRepository = require('../data/reactionRoleMessagesRepo
 const reactionRoles = require('../static-data/reaction-roles.json');
 
 module.exports = {
-	name: Events.MessageReactionAdd,
+	name: Events.MessageReactionRemove,
 	async execute(interaction, user) {
 		if (! await reactionRoleMessagesRepository.isReactionRoleMessage(interaction.message.id)) return;
 
 		const role = reactionRoles[interaction._emoji.name];
-		if (!role) {
-			console.log('[WARNING] Member reacted to a reaction role message with an unhndled emoji. Check channel permissions.');
-			return;
-		};
+		if (!role) return;
 
-		console.log(`[LOG] @${user.username} reacted to a reaction role message.`);
+		console.log(`[LOG] @${user.username} removed a reaction from a reaction role message.`);
 
 		const guildMember = await interaction.message.guild.members.fetch(user);
-		guildMember.roles.add(reactionRoles[interaction._emoji.name]);
+		guildMember.roles.remove(reactionRoles[interaction._emoji.name]);
 	}
 };
