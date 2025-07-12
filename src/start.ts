c6fa6cad82c349b3cb931c77860bab1bb66cee25
Ondřej Schlaichert <schlaichert.o@gmail.com>
2025-07-12 16:53:15 +0200

    Small changes

--- src/start.ts
+++ src/start.ts
@@ -1,18 +1,22 @@
 import { Client } from "discord.js";
 import config from "config";
-import logger from './utils/logger.js';
+import logger from "./utils/logger.js";
 import manifest from "../package.json" with { type: "json" };
 
-start();
+try {
+  await start();
+} catch (error) {
+  logger.error("An error occurred during startup:", error);
+}
 
 async function start(): Promise<void> {
-	logger.info(`starting version ${manifest.version}...`);
+  logger.info(`Starting version ${manifest.version}...`);
 
   const client = new Client({
     intents: [],
   });
-	
+
   client.login(config.get<string>("token"));
 
-	logger.info("successfully started!");
+  logger.info("Successfully started!");
 }
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -3,7 +3,8 @@ import manifest from "../../package.json" with { type: "json" };
 import config from "config";
 
 export default new Logger({
-	name: manifest.name,
-	type: "pretty",
-	minLevel: config.get<number>("minLogLevel") || 3,
+  name: manifest.name,
+  type: "pretty",
+  minLevel: config.get<number>("minLogLevel") || 3,
+  hideLogPositionForProduction: true,
 });
