8107d9f0c3b7e9ce3f2a885ba9dee3fc4e9dfe85
Ondřej Schlaichert <schlaichert.o@gmail.com>
2025-07-12 16:44:45 +0200

    Added basic project structure

--- src/start.ts
+++ src/start.ts
@@ -1,1 +1,18 @@
-console.log("good evening gentlemen");
+import { Client } from "discord.js";
+import config from "config";
+import logger from './utils/logger.js';
+import manifest from "../package.json" with { type: "json" };
+
+start();
+
+async function start(): Promise<void> {
+	logger.info(`starting version ${manifest.version}...`);
+
+  const client = new Client({
+    intents: [],
+  });
+	
+  client.login(config.get<string>("token"));
+
+	logger.info("successfully started!");
+}
---
+++ src/utils/logger.ts
@@ -0,0 +1,9 @@
+import { Logger } from "tslog";
+import manifest from "../../package.json" with { type: "json" };
+import config from "config";
+
+export default new Logger({
+	name: manifest.name,
+	type: "pretty",
+	minLevel: config.get<number>("minLogLevel") || 3,
+});
