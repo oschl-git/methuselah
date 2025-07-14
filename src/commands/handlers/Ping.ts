34bd176c5d45458e6aa34d394c0a15dbcc2ba3b2
Ondřej Schlaichert <schlaichert.o@gmail.com>
2025-07-14 18:10:36 +0200

    Tweaks to start script

--- src/commands/handlers/Ping.ts
+++ src/commands/handlers/Ping.ts
@@ -3,7 +3,7 @@ import Command from "./Command.js";
 import manifest from "../../../package.json" with { type: "json" };
 
 export default class Ping implements Command {
-  data = new SlashCommandBuilder().setDescription("Replies with Pong!");
+  data = new SlashCommandBuilder().setDescription("A simple testing command");
   async execute(interaction: ChatInputCommandInteraction): Promise<void> {
     await interaction.reply({
       content: `*Ho, ${interaction.user}! So you're still alive.*\n\`version: ${manifest.version}\``,
--- src/start.ts
+++ src/start.ts
@@ -1,24 +1,23 @@
-import { Client, Events } from "discord.js";
+import { Client } from "discord.js";
 import * as commandProcessor from "./commands/commandProcessor.js";
 import config from "config";
 import logger from "./utils/logger.js";
 import manifest from "../package.json" with { type: "json" };
 
-start();
+logger.info(`Starting ${manifest.name} ${manifest.version}...`);
 
-async function start(): Promise<void> {
-  logger.info(`Starting ${manifest.name} ${manifest.version}...`);
+const client = new Client({
+  intents: [],
+});
 
-  const client = new Client({
-    intents: [],
-  });
+logger.info("Loading commands...");
+commandProcessor.loadCommands(client);
 
-  logger.info("Loading commands...");
-  commandProcessor.loadCommands(client);
+await client.login(config.get<string>("token"));
 
-  client.on(Events.ClientReady, (readyClient) => {
-    logger.info(`Logged in as [${readyClient.user.tag}]`);
-  });
+await new Promise<void>((resolve) => {
+  if (client.isReady()) return resolve();
+  client.once("ready", () => resolve());
+});
 
-  client.login(config.get<string>("token"));
-}
+logger.info(`Logged in as [@${(client as Client<true>).user.tag}]`);
