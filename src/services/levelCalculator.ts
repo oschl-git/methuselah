0960bd853f21ba5c725f9019bf936c9b74beddf8
Ondřej Schlaichert <schlaichert.o@gmail.com>
2025-07-27 16:07:16 +0200

    Implemented function for calculating level

--- src/services/levelCalculator.ts
+++ src/services/levelCalculator.ts
@@ -106,3 +106,29 @@ function calculateXp(interactions: UserInteraction[]): number {
 
   return base + bonus;
 }
+
+export function calculateLevel(xp: number): number {
+  let low = 0;
+  let high = 10000;
+
+  while (low < high) {
+    const mid = Math.floor((low + high) / 2);
+    const totalXP = getTotalXpForLevel(mid);
+
+    if (totalXP > xp) {
+      high = mid;
+    } else {
+      low = mid + 1;
+    }
+  }
+
+  return low - 1;
+}
+
+export function getTotalXpForLevel(level: number): number {
+  let xp = 0;
+  for (let i = 0; i < level; i++) {
+    xp += 5 * i ** 2 + 50 * i + 100;
+  }
+  return xp;
+}
